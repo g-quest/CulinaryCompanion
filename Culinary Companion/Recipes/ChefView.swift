73be25a3a59b70dbc2cb141abeadf934e0906094
Greg Cuesta <gregory.cuesta@gmail.com>
2023-12-08 01:06:45 -0800

    chef page

--- Culinary Companion/Recipes/ChefView.swift
+++ Culinary Companion/Recipes/ChefView.swift
@@ -16,58 +16,90 @@ struct ChefRecipe {
 
 struct ChefView: View {
     @Environment(\.openWindow) private var openWindow
+    var recipes = ["c1", "c2", "c3"]
     
-    let recipes = [
-        ChefRecipe(title: "Apple Muffins", imageName: "apple-muffins-1"),
-        ChefRecipe(title: "Chicken Curry", imageName: "apple-muffins-1"),
-        
-    ]
-    
-    // Grid layout configuration
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    private let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     
     var body: some View {
-        
-        HStack(spacing: 20) {
-            // Chef's bio on the left
-            VStack(alignment: .leading) {
-                Text("Chef's Name")
-                    .font(.title)
-                    .bold()
-                Text("Short Bio of the Chef...")
-                    .font(.body)
-                Button("Watch Video") {
-                       openWindow(id: "RamsayVideo")
-                }
-            }
-            .padding()
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-            
-            VStack {
-                Text("Top Dishes")
-                    .font(.title)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(recipes, id: \.title) { recipe in
-                            VStack {
-                                Image(recipe.imageName)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 150)
-                                    .cornerRadius(10)
-                                Text(recipe.title)
-                            }
-                        }
+        NavigationView {
+            ScrollView {
+                
+                HStack(spacing: 0) {
+                    VStack(alignment: .center) {
+                        Image("gramsay")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fill)
+                            .clipped()
+                            .padding()
+                            .frame(width: 225, height: 225)
+                        
+                        
                     }
-                    .padding()
+                    
+                    Text("Gordon James Ramsay is a British celebrity chef, restaurateur, television presenter, and writer. His restaurant group, Gordon Ramsay Restaurants, was founded in 1997 and has been awarded seventeen Michelin stars overall and currently holds seven. After rising to fame on the British television miniseries Boiling Point in 1999, Ramsay became one of the best-known and most influential chefs in the world.")
+                        .font(.body)
                 }
-                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding([.leading, .trailing], 16)
+              
+                
+                
+                Section(header: Text("Upcoming Events")
+                    .font(.title)) {
+                        HStack() {
+                            VStack {
+                                Text("12/9/2023 | Chef Ramsay's Immersive Master Class")
+                                    .font(.title2)
+                                    .multilineTextAlignment(.center)
+                                Text("Join the chef himself as he takes a deep dive on what it takes to make the perfect dish.")   
+                                    .font(.body)
+                                    .multilineTextAlignment(.center)
+                                    .padding(20)
+                                Button("Watch Preview") {
+                                    openWindow(id: "RamsayVideo")
+                                }
+                                .padding(.top, 16)
+                            }
+                            
+                            Image("preview")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(16)
+                            
+                        }
+                        .padding(20)
+                        .border(Color.gray, width: 1)
+                    }
+                    .padding(.top, 32)
+                
+                Section(header: Text("Featured Recipes")
+                    .font(.title)) {
+                        VStack {
+                            ScrollView {
+                                LazyVGrid(columns: columns, spacing: 5) {
+                                    ForEach(recipes, id: \.self) { item in
+                                        Image(item)
+                                            .resizable()
+                                            .aspectRatio(1, contentMode: .fill)
+                                            .clipped()
+                                    }
+                                }
+                            }
+                            Button("View More") {
+                                
+                            }
+                            .padding([.top, .bottom], 16)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(.top, 32)
+                    .padding([.leading, .trailing], 100)
+                
+                
             }
-
+            .navigationBarTitle("Gordon Ramsay")
+            .padding([.leading, .trailing], 100)
         }
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- Culinary Companion/Tutorials/Tutorials.swift
+++ Culinary Companion/Tutorials/Tutorials.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct Tutorials: View {
+    
+    let tutorials = ["Home Gardening for Cooking", "Cutting Techniques", "Food Presentation and Plating", "Baking Fundamentals", "Preservation Techniques", "Mixology and Beverage Making", "Food Safety and Handling", "Nutrition and Diet", "Cooking for Special Occasions"]
+    let tutorialImages = ["garden", "knives", "plating", "baking", "preservation", "mixology", "safety", "nutrition", "occasions"]
+    
+    let columns = Array(repeating: GridItem(.flexible(), spacing: 24), count: 3)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 24) {
+                    ForEach(tutorials.indices, id: \.self) { index in
+                        let tutorial = tutorials[index]
+                        let image = (index < tutorialImages.count) ? tutorialImages[index] : "defaultImage" // Fallback image if not enough images
+                        
+                        ZStack {
+                            Image(image)
+                                .resizable()
+                                .aspectRatio(1, contentMode: .fill)
+                                .cornerRadius(10)
+                                .clipped()
+                                .overlay(
+                                    Rectangle()
+                                        .foregroundColor(.black)
+                                        .opacity(0.45)
+                                        .cornerRadius(10)
+                                )
+                            Text(tutorial)
+                                .font(.title)
+                                .frame(width: 190, height: 200, alignment: .center)
+                                .multilineTextAlignment(.center)
+                                .lineLimit(nil)
+                                .padding()
+                        }
+                    }
+                }
+                .padding(.top, 16)
+                .padding(.bottom, 32)
+                .padding([.leading, .trailing], 32)
+            }
+            .navigationBarTitle("Tutorials")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
+
+// Make sure to define the Recipe view or the destination view for the NavigationLink.
+
 
 #Preview {
     Tutorials()
